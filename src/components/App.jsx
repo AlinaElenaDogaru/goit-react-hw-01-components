1029f4f7660aa838f4a54734d96f2de61cdb191b
Alina Dogaru <alina.elena_dogaru@yahoo.com>
2024-10-06 21:13:05 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import Profile from './profile/Profile.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const App = () => {
   return (
     <div>
-      <Profile />
+      <BrowserRouter basename= { isProduction ? '/goit-react-hw-01' : ''}>
+        <Profile />
+        </BrowserRouter>
     </div>
   );
 };
