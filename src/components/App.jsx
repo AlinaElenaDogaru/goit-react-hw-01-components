1a4e8aab7f30dda124614be10e9e70d1e115648f
Alina Dogaru <alina.elena_dogaru@yahoo.com>
2024-10-06 23:10:49 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import Profile from './profile/Profile.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import user from './user.json';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -7,7 +8,12 @@ export const App = () => {
   return (
     <div>
       <BrowserRouter basename={isProduction ? '/goit-react-hw-01-components' : ''}>
-        <Profile />
+        <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}/>
         </BrowserRouter>
     </div>
   );
